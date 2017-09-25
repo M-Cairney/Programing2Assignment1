945fe6f6a8f21cd140a99d766dfc3316fbea54ae
M-Cairney <mitchell.cairney@my.jcu.edu.au>
2017-09-25 12:21:53 +1000

    still trying to get jetwall to work

--- JetWall.java
+++ JetWall.java
@@ -43,7 +43,7 @@ public class JetWall extends JComponent {
         for (Line line : lines) {
             g.setColor(line.color);
             g.drawLine(line.x1, line.y1, line.x2, line.y2);
-            g.setVisible(true);
+          
         }
     }
 }
